4dab644f4fd6e46ec0f1c56362bad256fec5dddd
Jacob Stenson <jacob@nosnets.co.uk>
2019-04-27 22:52:09 +0100

    Revamp of map. Hover on ground pieces. Place Cage and Dragon on one click.
    Revamp of the map system so a cage is placed on one piece of ground.
    When the user hovers on a ground piece, the ground will go darkier.
    Clicking a ground piece places a Cage and a Dragon inside that Cage.

---
+++ DragonSanctuary3D/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    bool isPlacingSomething = true;
+    bool placingDragon = true;
+
+    public GameObject smallcageprefab;
+    public GameObject redDragonPrefab;
+
+    public static BuildManager instance;
+
+
+    //--
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (isPlacingSomething)
+            {
+                
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider != null)
+                    {
+                        // If the user clicked on the terrain
+                        if (hit.collider.gameObject.tag == "Terrain")
+                        {
+                            // click hit terrain.
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void ToggleBuldingPlacing()
+    {
+        isPlacingSomething = !isPlacingSomething;
+    }
+    public void ToggleDragonPlacing()
+    {
+        placingDragon = !placingDragon;
+    }
+
+    public bool getPlacingStatus()
+    {
+        return isPlacingSomething;
+    }
+
+    public bool getDragonPlacingStatus()
+    {
+        return placingDragon;
+    }
+
+    public void getWhatToPlace()
+    {
+        //
+    }
+
+    public GameObject getDragon()
+    {
+        return redDragonPrefab;
+    }
+
+    public GameObject getCage()
+    {
+        return smallcageprefab;
+    }
+}
---
+++ DragonSanctuary3D/Assets/Scripts/Ground.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ground : MonoBehaviour
+{
+    public bool isBuildingPlacedOnGround;
+    public bool isDragonPlaced;
+
+    public Material hoverMaterial;
+
+    BuildManager buildManager;
+    Material defaultMaterial;
+    Renderer rend;
+
+    public string buildingPlacedOn;
+
+    //Vector3 dragonPlaceRotationOffsetT = new Vector3(0.0f, -90.0f, 0.0f);
+    Quaternion dragonPlaceRotationOffset = new Quaternion(0.0f, -90.0f, 0.0f, 1.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+
+        rend = GetComponent<Renderer>();
+        defaultMaterial = rend.material;
+
+        isBuildingPlacedOnGround = false;
+    }
+
+    private void OnMouseDown()
+    {
+        bool buildingPlacingStatus = buildManager.getPlacingStatus();
+
+        if (!isBuildingPlacedOnGround && buildingPlacingStatus)
+        {
+            PlaceBuilding(buildManager.getCage(),"Small Cage");
+        }
+
+
+        bool doPlaceDragon = buildManager.getDragonPlacingStatus();
+
+        //
+        if (buildingPlacedOn == "Small Cage" && doPlaceDragon)
+        {
+            PlaceDragon(buildManager.getDragon());
+        }
+
+    }
+
+    //when the user hovers on the ground
+    private void OnMouseEnter()
+    {
+        bool buildingPlacingStatus = buildManager.getPlacingStatus();
+
+        if (isBuildingPlacedOnGround || !buildingPlacingStatus)
+            return;
+        rend.material = hoverMaterial;
+        
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material = defaultMaterial;
+        //when they move outside.
+    }
+
+    void PlaceBuilding(GameObject objToPlace,String whatPlaced)
+    {
+        //If hovering over something else, exit function.
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+
+        GameObject smallCage = (GameObject)Instantiate(objToPlace, transform.position, transform.rotation);
+        rend.material = defaultMaterial;
+        buildingPlacedOn = whatPlaced;
+
+        isBuildingPlacedOnGround = true;
+    }
+
+    void PlaceDragon(GameObject objToPlace)
+    {
+        Debug.Log("Rotation...");
+        Debug.Log(transform.rotation);    
+
+        GameObject DragonInCage = (GameObject)Instantiate(objToPlace, transform.position, transform.rotation);
+
+        // Rotates dragon in cage 90o to the left.
+        DragonInCage.transform.eulerAngles = new Vector3(
+        DragonInCage.transform.eulerAngles.x,
+        DragonInCage.transform.eulerAngles.y + -90,
+        DragonInCage.transform.eulerAngles.z
+        );
+
+        isDragonPlaced = true;
+    }
+}
