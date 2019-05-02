da791b906271cbdc7b0c49acd0a11d4962aa08e3
Jacob Stenson <jacob@nosnets.co.uk>
2019-05-02 16:32:19 +0100

    Name change
    Name has been changed to remove the '3D' part as it felt unprofessional and irrelevant due to the 2D version of the game being discontinued.

--- DragonSanctuary/Assets/Scripts/Shop.cs
+++ DragonSanctuary/Assets/Scripts/Shop.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
    
+    // When the user clicks on something in the shop, deduct gold from them and then call GROUND.PLACESOMETHING
     public void BuySomething()
     {
 
